4b5a41d5c8b939daf381676d4b9205395df3d409
Dominik <domx2251@gmail.com>
2025-10-12 18:55:12 +0200

    Setup projektu react - vite / tailwindcss / flowbite

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { Alert } from 'flowbite-react'
+
+function App() {
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <Alert color="info">Hello World</Alert>
+    </div>
+  )
+}
+
+export default App
